9545a3e0af66d2c6aaafec9b4078e586696d785a
Tilak Dave <davetilak003@gmail.com>
2021-04-14 00:52:48 +0530

    accept post request for signup

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -13,4 +14,19 @@ def logout(request):
     return HttpResponse('Hello from logout page')
 
 def signup(request):
+    if request.method=='POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        f_name = request.POST['f_name']
+        l_name = request.POST['l_name']
+        is_sf = request.POST['is_sf']
+        password = request.POST['pass']
+        conf_password = request.POST['conf_pass']
+        if len(username) < 3:
+            return redirect(signup)
+        if password != conf_password:
+            return redirect(signup)
+        new_user = authenticate(email=email, username=username, password=conf_password)
+        login(request, new_user)
+        
     return HttpResponse('Hello from signup page')
