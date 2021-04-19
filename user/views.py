7d27a7c7c61dc12ab4aeeca00f0f4877cd67363a
Tilak Dave <davetilak003@gmail.com>
2021-04-19 23:55:34 +0530

    resume form complete

--- user/views.py
+++ user/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from .forms import SignupForm, LoginForm
 from django.contrib.auth import authenticate, login, logout
 from .models import User, Portfolio, Resume, Skill, Job, Education, Accomplishments, Company
+from datetime import datetime
 
 
 # Create your views here.
@@ -33,7 +34,63 @@ def account(request):
                 context = {'company':company}
             except:
                 context = {}
-    
+        else:
+            if request.POST:
+                name = request.POST.get('name')
+                if name == 'edu_form':
+                    college = request.POST['college']
+                    stream = request.POST['stream']
+                    start_year = str(request.POST['start_year'])[:4]
+                    end_year = str(request.POST['end_year'])[:4]
+                    status = request.POST['status']
+                    if status=="on":
+                        status = True
+                    else:
+                        status = False
+                    new_education = Education(user=request.user, status=status, college=college,  start_year=int(start_year), end_year=int(end_year) ,stream=stream)
+                    new_education.save()
+                    return redirect('account')
+                elif name=='job_form':
+                    profile = request.POST['profile']
+                    organization = request.POST['organization']
+                    location = request.POST['location']
+                    job_desc = request.POST['job_desc']
+                    start_year = datetime.strptime(request.POST['start_year'], "%Y-%m-%d")
+                    end_year = datetime.strptime(request.POST['end_year'], "%Y-%m-%d")
+                    new_job = Job(user=request.user, profile=profile, organization=organization, location=location, start_date=start_year, end_date=end_year ,job_desc=job_desc)
+                    new_job.save()
+                    return redirect('account')
+                elif name=='port_form':
+                    proj_name = request.POST['proj_name']
+                    link = request.POST['link']
+                    proj_desc = request.POST['proj_desc']
+                    start_year = datetime.strptime(request.POST['start_year'], "%Y-%m-%d")
+                    end_year = datetime.strptime(request.POST['end_year'], "%Y-%m-%d")
+                    new_port = Portfolio(user=request.user, proj_name=proj_name, link=link, proj_desc=proj_desc, start_date=start_year, end_date=end_year)
+                    new_port.save()
+                    return redirect('account')
+                elif name=='skill_form':
+                    skill_name = request.POST['skill_name']
+                    skill_level = request.POST['skill_level']
+                    new_skill = Skill(user=request.user, skill_name=skill_name, skill_level=skill_level)
+                    new_skill.save()
+                    return redirect('account')
+                elif name=='acomp_form':
+                    desc = request.POST['desc']
+                    new_acomp = Accomplishments(user=request.user,desc=desc)
+                    new_acomp.save()
+                    return redirect('account')
+                elif name=='res_form':
+                    print('entered')
+                    bio = request.POST['bio']
+                    avatar = request.POST['avatar']
+                    ph_no = request.POST['ph_no']
+                    primary_city = request.POST['pri_city']
+                    secondary_city = request.POST['sec_city']
+                    new_resume = Resume(user=request.user, desc=bio, avatar=avatar, phn_no=ph_no, primary_city=primary_city, secondary_city=secondary_city)
+                    new_resume.save()
+                    print('saved')
+                    return redirect('account')
         return render(request, 'user/account.html', context)
     return redirect('login')
 
